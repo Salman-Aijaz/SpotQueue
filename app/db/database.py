bcfeeb87f914a10bd645723281058d4f632f38a3
Salman-Aijaz <salman333699@gmail.com>
2024-10-23 18:25:51 +0500

    redis  setup with docker

--- app/db/database.py
+++ app/db/database.py
@@ -2,14 +2,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.exc import SQLAlchemyError
 from app.core.config import settings
-
+import redis 
 
 # Create SQLAlchemy engine and sessionmaker 
 engine = create_engine(settings.DATABASE_URL, echo=True) 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
+# Initialize Redis client
+redis_client = redis.StrictRedis(host='localhost', port=6379, decode_responses=True)
+
 # Base class for models
 Base = declarative_base()
+
+# Optionally, check if Redis is running
+try:
+    redis_client.ping()
+    settings.logger.info("Connected to Redis")
+except redis.ConnectionError:
+    settings.logger.error("Could not connect to Redis")
 
 # Function to initialize the database with error handling
 def init_db():
